1dd24096c687d97b5eb60f968d9f6461450bac02
tcrouzet <tc@tcrouzet.com>
2024-12-24 14:28:43 +0100

    First

--- main.ts
+++ main.ts
@@ -1,84 +1,243 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, TFolder, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface FolderFusionSettings {
+    noteSeparator: string;
+	exclude: string;
+	smallcaps: boolean;
+	right: boolean;
+	newpage: boolean;
+	toppara: boolean;
+	poem: boolean;
+	hardspaces: boolean;
+	nocomments: boolean;
+	noMDcomments: boolean;
+	apostrophe: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: FolderFusionSettings = {
+	noteSeparator: '',
+	exclude: "sortspec|_i",
+	smallcaps: false,
+	right: false,
+	newpage: false,
+	toppara: false,
+	poem: false,
+	hardspaces: false,
+	nocomments: true,
+	noMDcomments: true,
+	apostrophe: true
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class FolderFusion extends Plugin {
+	settings: FolderFusionSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new FolderFusionSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		this.registerEvent(this.app.workspace.on('file-menu', (menu, file) => {
+			// Vérifier si l'élément cliqué est un dossier
+			if (file instanceof TFolder) {
+				// Ajouter une commande d'exportation au menu contextuel
+				menu.addItem((item) => {
+					item.setTitle('Folder fusion')
+					   .onClick(() => {
+						   // Appeler votre fonction d'exportation ici
+						   this.exportFolder(file.path);
+					   });
+				});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+				/*menu.addItem((item) => {
+                    item.setTitle('Duplicate Folder')
+                        .onClick(() => {
+                            // Appeler la fonction de duplication ici
+                            this.duplicateFolder(file.path);
+                        });
+                });*/
+			}
+		}));
 	}
 
-	onunload() {
+	async exportFolder(folderPath:string){
+		this.concatenateFiles(folderPath).then(result => {
+			this.savingFiles(folderPath,result)
+		});
+	}
+
+	async concatenateFiles(folderPath: string): Promise<string> {
+
+		const fusionFileRegex = /fusion-.*-\d{8}-\d{4}\.md$/;
+		const excludeRegex = new RegExp(this.settings.exclude);
+
+		const files = this.app.vault.getFiles()
+			.filter(file => 
+				file.path.startsWith(folderPath) && 
+				!fusionFileRegex.test(file.path)
+			);
+
+		const sortedFiles = files.sort((a, b) => a.path.localeCompare(b.path));
+
+		let concatenatedContent = '';
+	
+		for (const file of sortedFiles) {
+
+			if (excludeRegex.test(file.path)) {
+				continue;
+			}
+
+			let fileContent = await this.app.vault.read(file);
+
+			if(this.settings.nocomments){
+				fileContent = fileContent.replace(/<!--[\s\S]*?-->/g, '');
+			}
+
+			if(this.settings.noMDcomments){
+				fileContent = fileContent.replace(/%%[\s\S]*?%%\n?/g, '');
+			}
+
+			if(this.settings.hardspaces){
+				fileContent = this.hardspaces(fileContent)
+			}
+
+			if(this.settings.apostrophe){
+				fileContent = this.apostrophes(fileContent);
+			}
+
+			if(this.settings.smallcaps){
+				fileContent = fileContent.replace(/--(.*?)--/g, '<span style="font-variant:small-caps">$1</span>');
+			}
+
+			if(this.settings.right){
+				fileContent = fileContent.replace(/@@(.*?)(\n|$)/g, '<div custom-style="Right" class="right" align="right">$1</div>\n');
+			}
+
+			if(this.settings.toppara){
+				fileContent = fileContent.replace(/££(.*?)(\n|$)/g, '<div custom-style="top-para" class="top-para" style="text-align:justify">$1</div>\n');
+			}
+
+			if(this.settings.poem){
+				//fileContent = fileContent.replace(/€€([\s\S]*?)(?=\n\n|$)/g, '<div custom-style="poem" class="poem">$1</div>\n');
+				fileContent = this.poem(fileContent);
+			}
+
+			if(this.settings.newpage){
+				fileContent = fileContent.replace(/\\newpage/g, '<div custom-style="page-break" class="page-beak" style="page-break-after: always">-</div>');
+			}
+
+			concatenatedContent += fileContent + '\n\n'; // Ajouter une séparation entre les fichiers
+			concatenatedContent += this.settings.noteSeparator;
+		}
+	
+		return concatenatedContent;
+	}
+
+	async savingFiles(folderPath: string,content: string) {
+		const folderName = this.getFolderNameFromPath(folderPath);
+		const currentDate = new Date();
+		const formattedDate = currentDate.toISOString().slice(0, 10).replace(/-/g, '') + '-' + currentDate.toISOString().slice(11, 13) + currentDate.toISOString().slice(14, 16);
+		const tempFilePath = `${folderPath}/fusion-${folderName}-${formattedDate}.md`;
+		await this.app.vault.adapter.write(tempFilePath, content);
+	}
+
+	getFolderNameFromPath(folderPath:string) {
+		const pathParts = folderPath.split('/');
+		return pathParts[pathParts.length - 1];
+	}
+
+	hardspaces(content: string) {
+		// Extraire les sections HTML et YAML
+		const htmlRegex = /<[^>]*>/g;
+		const yamlRegex = /---[\s\S]+?---/g;
+	
+		let htmlMatches = content.match(htmlRegex) || [];
+		let yamlMatches = content.match(yamlRegex) || [];
+	
+		// Remplacer ces sections par des marqueurs temporaires
+		content = content.replace(htmlRegex, 'HTML_PLACEHOLDER');
+		content = content.replace(yamlRegex, 'YAML_PLACEHOLDER');
+	
+		// Appliquer les règles d'espacement
+		let regex = /(.)([:;?!»])/g;
+		content = content.replace(regex, (match, p1, p2) => {
+			if (p1 === ' ' || p1 === '\u00A0') {
+				return '\u00A0' + p2;
+			}
+			return p1 + '\u00A0' + p2;
+		});
+	
+		regex = /([«—])(.)?/g;
+		content = content.replace(regex, (match, p1, p2) => {
+			if (p2 === ' ' || p2 === '\u00A0') {
+				return p1 + '\u00A0';
+			}
+			return p1 + '\u00A0' + p2;
+		});
+	
+		// Réintégrer les sections HTML et YAML
+		htmlMatches.forEach(placeholder => {
+			content = content.replace('HTML_PLACEHOLDER', placeholder);
+		});
+		yamlMatches.forEach(placeholder => {
+			content = content.replace('YAML_PLACEHOLDER', placeholder);
+		});
+	
+		return content;
+	}
+
+	apostrophes(content: string) {
+		// Extraire les sections HTML et YAML
+		const htmlRegex = /<[^>]*>/g;
+		const yamlRegex = /---[\s\S]+?---/g;
+	
+		let htmlMatches = content.match(htmlRegex) || [];
+		let yamlMatches = content.match(yamlRegex) || [];
+	
+		// Remplacer ces sections par des marqueurs temporaires
+		content = content.replace(htmlRegex, 'HTML_PLACEHOLDER');
+		content = content.replace(yamlRegex, 'YAML_PLACEHOLDER');
+
+		content = content.replace(/'/g, "’");
+	
+		// Réintégrer les sections HTML et YAML
+		htmlMatches.forEach(placeholder => {
+			content = content.replace('HTML_PLACEHOLDER', placeholder);
+		});
+		yamlMatches.forEach(placeholder => {
+			content = content.replace('YAML_PLACEHOLDER', placeholder);
+		});
+	
+		return content;
+	}
+
+	poem(content:string) {
+			content = content.replace(/€€(.*?)€€/gs, (match, poem) => {
+			let paragraphs = poem.split('\n\n');
+		
+			if (paragraphs.length === 1) {
+				return `<div custom-style="poem-solo" class="poem-solo">${paragraphs[0]}</div>\n`;
+			} else {
+				// Appliquer des styles différents au premier, dernier et aux paragraphes intermédiaires
+				let formattedPoem = paragraphs.map((p, index) => {
+					if (index === 0) {
+						return `<div custom-style="poem-top" class="poem-top">${p}</div>\n`;
+					} else if (index === paragraphs.length - 1) {
+						return `<div custom-style="poem-bottom" class="poem-bottom">${p}</div>`;
+					} else {
+						return `<div custom-style="poem" class="poem">${p}</div>\n`;
+					}
+				}).join('\n');
+		
+				return formattedPoem;
+			}
+		});
+		return content;
+	}
+
+	async duplicateFolder(folderPath:string) {
 
 	}
 
@@ -89,28 +248,16 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	onunload() {
+	}
+
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class FolderFusionSettingTab extends PluginSettingTab {
+	plugin: FolderFusion;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: FolderFusion) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,14 +267,118 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
+		const titleEl = containerEl.createEl('h2', { text: 'Folder Fusion settings' });
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Small caps')
+			.setDesc('Convert --text-- into Pandoc HTML small caps')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.smallcaps)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.smallcaps = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+		.setName('Apply right style')
+		.setDesc('Convert @@paragraph into Pandoc HTML align right')
+		.addToggle(toggle => toggle
+			.setValue(this.plugin.settings.right)
+			.onChange(async (value) => {
+				this.plugin.settings.right = value;
+				await this.plugin.saveSettings();
+			}));
+
+		new Setting(containerEl)
+		.setName('Applys top-para style')
+		.setDesc('Convert ££paragraph into Pandoc Docx top-para')
+		.addToggle(toggle => toggle
+			.setValue(this.plugin.settings.toppara)
+			.onChange(async (value) => {
+				this.plugin.settings.toppara = value;
+				await this.plugin.saveSettings();
+			}));
+
+		new Setting(containerEl)
+		.setName('Applys poem style')
+		.setDesc('Convert €€paragraph into Pandoc Docx poem')
+		.addToggle(toggle => toggle
+			.setValue(this.plugin.settings.poem)
+			.onChange(async (value) => {
+				this.plugin.settings.poem = value;
+				await this.plugin.saveSettings();
+			}));
+
+		new Setting(containerEl)
+		.setName('New page')
+		.setDesc('Convert \\newpage into word stype page-break')
+		.addToggle(toggle => toggle
+			.setValue(this.plugin.settings.newpage)
+			.onChange(async (value) => {
+				this.plugin.settings.newpage = value;
+				await this.plugin.saveSettings();
+			}));
+
+		new Setting(containerEl)
+		.setName('No HTML comments')
+		.setDesc('Remove all <!-- text -->')
+		.addToggle(toggle => toggle
+			.setValue(this.plugin.settings.nocomments)
+			.onChange(async (value) => {
+				this.plugin.settings.nocomments = value;
+				await this.plugin.saveSettings();
+			}));
+
+		new Setting(containerEl)
+		.setName('No Markdown comments')
+		.setDesc('Remove all %%text%%')
+		.addToggle(toggle => toggle
+			.setValue(this.plugin.settings.noMDcomments)
+			.onChange(async (value) => {
+				this.plugin.settings.noMDcomments = value;
+				await this.plugin.saveSettings();
+			}));
+
+	
+		new Setting(containerEl)
+		.setName('Typographic aposttrophes')
+		.setDesc('Replace \' with ’')
+		.addToggle(toggle => toggle
+			.setValue(this.plugin.settings.apostrophe)
+			.onChange(async (value) => {
+				this.plugin.settings.apostrophe = value;
+				await this.plugin.saveSettings();
+			}));
+
+		new Setting(containerEl)
+		.setName('French hard spaces')
+		.setDesc('Addapt hard spaces before double ponctuations')
+		.addToggle(toggle => toggle
+			.setValue(this.plugin.settings.hardspaces)
+			.onChange(async (value) => {
+				this.plugin.settings.hardspaces = value;
+				await this.plugin.saveSettings();
+			}));
+	
+		new Setting(containerEl)
+		.setName('Exclude')
+		.setDesc('Regex to exclude files and folders.')
+		.addText(text => text
+			.setPlaceholder('Enter your files and folders')
+			.setValue(this.plugin.settings.exclude)
+			.onChange(async (value) => {
+				this.plugin.settings.exclude = value;
+				await this.plugin.saveSettings();
+			}));
+
+		new Setting(containerEl)
+			.setName('Note separator')
+			.setDesc('You can use Latex \\newpage for example.')
+			.addText(text => text
+				.setPlaceholder('Enter your separator')
+				.setValue(this.plugin.settings.noteSeparator)
+				.onChange(async (value) => {
+					this.plugin.settings.noteSeparator = value;
 					await this.plugin.saveSettings();
 				}));
 	}
