ac3041d6c8281f23f9a373964cfa4c646e7e3bbd
tcrouzet <tc@tcrouzet.com>
2024-12-24 15:47:28 +0100

    Bugs

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#chmod +x release.sh
+
+
+# Variables
+REPO=$(jq -r '.repo' commit.json)
+TAG=$(jq -r '.version' manifest.json) 
+RELEASE_NAME="$TAG"
+DESCRIPTION="Release $TAG"
+FILES=("main.js" "manifest.json")
+
+# Créer la release
+gh release create "$TAG" "${FILES[@]}" \
+  --repo "$REPO" \
+  --title "$RELEASE_NAME" \
+  --notes "$DESCRIPTION"
